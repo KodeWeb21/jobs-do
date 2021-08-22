340ec3e2a9f9c4e2a47cd40a1577596d64a90069
kode21 <bryantmejia21@gmail.com>
2021-08-21 21:57:02 -0400

    logica para la parte de jobs

---
+++ js/job.js
@@ -0,0 +1,31 @@
+const contenedorVacantes = document.querySelector('.contenedor-vacantes');
+const url = 'http://127.0.0.1:5500/jobs.json';
+
+const obtenerTodosLosTrabajo = (url) =>{
+    return fetch(url)
+    .then(respuesta=>respuesta.json())
+    .then(jobs=>jobs)
+    .catch(error=>console.error(new Error(error)));
+}
+
+const pintarTrabajo = async () =>{
+    const trabajos = await obtenerTodosLosTrabajo(url);
+    const queryString = location.search;
+    const parametros = new URLSearchParams(queryString);
+    const idJob = parseInt(parametros.get('job'))
+    console.log(trabajos);
+    const trabajo = trabajos.filter((job)=>job.id === idJob);
+    console.log(trabajo);
+    const vacante = document.getElementById('vacante');
+    const tituloVacante = vacante.content.querySelector('.vacante__titulo');
+    const descripcionVacante = vacante.content.querySelector('.vacante__descripcion');
+    const vacanteBoton = vacante.content.querySelector('.vacante__boton');
+    tituloVacante.textContent = trabajo[0].puesto;
+    descripcionVacante.textContent = trabajo[0].descripcion;
+    vacanteBoton.setAttribute('href',trabajo[0].url)
+    vacanteBoton.textContent = 'Aplicar en Aldaba';
+    const clon = document.importNode(vacante.content,true);
+    contenedorVacantes.appendChild(clon);
+}
+
+pintarTrabajo();
